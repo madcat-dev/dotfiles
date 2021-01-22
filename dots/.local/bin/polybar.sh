de9161ed1bd89ea1efc266f79b751050defd3663
Madcat <virt780@gmail.com>
2021-01-22 17:29:05 +0200

    fix

---
+++ dots/.local/bin/polybar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Polybar panel
+export MONITOR=$(xrandr | grep " connected " | awk '{print $1}')
+
+
+export XRES=$(xrandr -q | grep '\*' | tr 'x' ' ' | awk '{print $1}')
+
+layout_size=330
+tray_size=330
+
+let "top_size = $XRES - $layout_size - $tray_size"
+let "tray_offset = $top_size + $layout_size"
+
+
+export LAYOUT_SIZE="${layout_size}px"
+export TRAY_SIZE="${tray_size}px"
+export TRAY_OFFSET="${tray_offset}px"
+export TOP_SIZE="${top_size}px"
+
+killall -u $USER polybar
+
+polybar top &
+polybar layouts &
+polybar tray &
+
+exit 0
