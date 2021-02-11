fe97f9bad12f4a74d5a08f6cd4d2eca4cb37fb61
Madcat <virt780@gmail.com>
2021-02-11 10:51:16 +0200

    fix

--- bin/backup.sh
+++ bin/backup.sh
@@ -4,8 +4,14 @@ DIR=$(dirname $0)
 cd $DIR
 
 PROFILE=${1:-$USER}
-let OFFSET="$(date +'%H * %M * %S')"
+
+DATE=(`echo "$(date +'%F:%H*%M*%S')" | sed 's/:/\n/g'`)
+let OFFSET="${DATE[1]}"
+
 DEST="backup/$(date +%F)/${PROFILE}@$(hostname):${OFFSET}"
+
+exit 0
+
 
 echo $DEST
 mkdir -p $DEST
