f0076a4e5f299efc1eafad631a7ec6d76d5aa810
Madcat <virt780@gmail.com>
2021-02-11 16:48:07 +0200

    fix

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
 DIR=$(dirname $0)
-cd $DIR
 
 PROFILE=${1:-$USER}
+
+if [[ ! -d $PROFILE ]]; then
+    PROFILE="/home/$PROFILE"
+
+    if [[ ! -d $PROFILE ]]; then
+        echo "Not found profile"
+        exit 1
+    fi
+fi
 
 DATE=(`echo "$(date +'%F:%H*%M*%S')" | sed 's/:/\n/g'`)
 let OFFSET="${DATE[1]}"
 
-DEST="backup/$(date +%F)/${PROFILE}@$(hostname):${OFFSET}"
+DEST="backup/$(date +%F)/$(basename $PROFILE)@$(hostname):$OFFSET"
+echo $PROFILE
+echo $DEST
 
 exit 0
 
 
-echo $DEST
+cd $DIR
 mkdir -p $DEST
 
